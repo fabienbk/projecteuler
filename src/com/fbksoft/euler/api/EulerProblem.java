76e9e22f6292534308b0987741af3f6f46b92e1f
Fabien Benoit-Koch <fabien.benoit@gmail.com>
2014-09-09 14:59:16 +0200

    Problem 8 and 9

---
+++ src/com/fbksoft/euler/api/EulerProblem.java
@@ -0,0 +1,14 @@
+package com.fbksoft.euler.api;
+
+public abstract class EulerProblem {
+
+	public abstract Object getSolution();
+
+	public void execute() {
+		long t1 = System.currentTimeMillis();
+		String solution = getSolution().toString();
+		long t2 = System.currentTimeMillis();
+		System.out.println("Solution = " + solution + " . solved in " + (t2-t1) + " ms");
+	}
+	
+}
