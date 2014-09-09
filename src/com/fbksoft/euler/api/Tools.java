aa09e42a70430e12562816e203d382c252c8b786
Fabien Benoit-Koch <fabien.benoit@gmail.com>
2014-09-09 13:45:32 +0200

    better prime testing algorithm

--- src/com/fbksoft/euler/api/Tools.java
+++ src/com/fbksoft/euler/api/Tools.java
@@ -19,11 +19,29 @@ public class Tools {
 	}
 	
 	public static boolean isPrime(Long n) {
-		for(Long i = 2L; i <= Math.sqrt(n); i++) {
-			if (n%i==0) {
-				return false;
-			}
+		if (n == 1) {
+			return false;
 		}
+		if (n < 4) {
+			return true;
+		}
+		if (n % 2 == 0) {
+			return false;
+		}
+		if (n < 9) {
+			return true;
+		}
+		if (n%3==0) {
+			return false;
+		}
+		
+		int f = 5;
+		while(f <= (long)Math.sqrt(n)) {
+			if (n%f==0) return false;
+			if (n%(f+2)==0) return false;
+			f=f+6;
+		}
+	
 		return true;
 	}
 	
@@ -51,7 +69,12 @@ public class Tools {
 
 
 	public static void main(String[] args) {
-		System.out.println(getPrimeDecomposition(234L));
+		for(long i = 0; i < 1000; i++) {
+			if (isPrime(i)) {
+	
+				System.out.println(i);
+			}
+		}
 	}
 
 
--- src/com/fbksoft/euler/problem7/The10001Prime.java
+++ src/com/fbksoft/euler/problem7/The10001Prime.java
@@ -3,12 +3,12 @@ package com.fbksoft.euler.problem7;
 import com.fbksoft.euler.api.Tools;
 
 public class The10001Prime {
-
+	
 	public static void main(String[] args) {
 		long t1 = System.currentTimeMillis();
 		int i = 1;
-		long n = 2;
-		while(i < 10001) {
+		long n = 1;
+		while(i <= 10001) {
 			if (Tools.isPrime(++n)) {
 				i++;
 			}
@@ -17,5 +17,4 @@ public class The10001Prime {
 		long t2 = System.currentTimeMillis();
 		System.out.println(n + " found in " + (t2 - t1) + "ms");
 	}
-	
 }
